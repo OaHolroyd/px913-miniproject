f053a67674a76edac136fe36e8dd94a8fd79e6fb
oh <oaholroyd@gmail.com>
2021-11-30 21:30:34 +0000

    added args to main program

--- src/particle.f90
+++ src/particle.f90
@@ -1,29 +1,19 @@
 program particle
-  use command_line
+  use model_data
+  use read_input
 
   implicit none
 
-  integer :: nx, ny
-  character(len=8) :: problem
+  integer :: err ! error flag
 
-  ! Default arguments
-  nx = 10
-  ny = 15
-  problem = "null"
+  ! read and validate command line args
+  call init_args(err)
+  if (err /= 0) stop
+  call validate_args(err)
+  if (err /= 0) stop
 
-  ! Parse all the command line args and read them
-  CALL parse_args()
-
-  IF (.NOT.get_arg("nx", nx)) THEN
-    PRINT"(A, I0)", "WARNING: argument 'nx' not received, defaulting to ", nx
-  ENDIF
-
-  IF (.NOT.get_arg("ny", ny)) THEN
-    PRINT"(A, I0)", "WARNING: argument 'ny' not received, defaulting to ", ny
-  ENDIF
-
-  IF (.NOT.get_arg("problem", problem)) THEN
-    PRINT"(A, A)", "WARNING: argument 'init' not received, defaulting to ", problem
-  ENDIF
+  print*, nx
+  print*, ny
+  print*, problem
 
 end program particle
